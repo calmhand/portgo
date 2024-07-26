6d34663a6735f1591e72df62523ff581f5d2e51e
johnny <52714929+calmhand@users.noreply.github.com>
2024-07-26 17:28:54 -0400

    +chrome manifest setup; +tailwind; +basic markup; +npm init; ...init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+        'black': '#1e1e1e',
+        'white': '#F2ECE3',
+        'green': '#276045'
+    }
+  },
+  plugins: [],
+}
+
